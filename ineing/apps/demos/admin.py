5d2930bfc2893222d303ff85405039bcf726ed7f
Jose Ramirez <jcramireztello@gmail.com>
2021-09-21 23:36:33 -0500

    feat: add admin for demo app

--- ineing/apps/demos/admin.py
+++ ineing/apps/demos/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Categoria, Item
 
-# Register your models here.
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'codigo', 'nombre', 'price', 'unidad_medida')
+    list_filter = ('unidad_medida', )
+    list_editable = ('price', )
