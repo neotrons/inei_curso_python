c9b52d976a1cf657cc2f1a121490823331a4a871
Jose Ramirez <jcramireztello@gmail.com>
2021-09-25 08:55:17 -0500

    feat: custom table and field
    - add model using legacy table

--- ineing/apps/demos/models.py
+++ ineing/apps/demos/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 
+class UnidadMedida(models.Model):
+    id = models.AutoField(primary_key=True, db_column='id')
+    name = models.CharField(max_length=100, db_column='nombre')
+
+    class Meta:
+        db_table = "UNIDAD_MEDIDA"
+
+    def __str__(self):
+        return self.name
+
+
 class Categoria(models.Model):
     nombre = models.CharField(max_length=100)
 
@@ -31,3 +42,13 @@ class Item(models.Model):
 
     def __str__(self):
         return self.codigo
+
+
+class Proveedor(models.Model):
+    codigo = models.AutoField(db_column='CODIGO', primary_key=True)  # Field name made lowercase.
+    nombre = models.CharField(db_column='NOMBRE', max_length=100)  # Field name made lowercase.
+    ruc = models.CharField(db_column='RUC', max_length=13)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'PROVEEDOR'
